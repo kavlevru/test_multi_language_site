7913ec3a5f57e9fc365d9ef4208a32e86654fbc2
kavlevru <i@kavlev.ru>
2022-07-02 16:05:42 +0300

    final version for stepik lesson

--- conftest.py
+++ conftest.py
@@ -2,25 +2,31 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+
+# Добавление аргументов для запуска тестов
 def pytest_addoption(parser):
     parser.addoption('--browser_name', default='chrome', action='store', help='choose browser firefox or chrome')
-    parser.addoption('--language', default='fr', action='store', help='Choose language for testing')
+    parser.addoption('--language', default=None, action='store', help='Choose language for testing')
+
 
 @pytest.fixture(scope='function')
 def browser(request):
-    browser_name = request.config.getoption('browser_name')
     user_language = request.config.getoption('language')
+    if user_language is None:
+        raise pytest.UsageError('--language must have the value es, fr, ru or other')
+    browser_name = request.config.getoption('browser_name')
     browser = None
     if browser_name == 'chrome':
-        #код определения языка для хрома
+        # код определения языка для Chrome
         options = Options()
         options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
         browser = webdriver.Chrome(options=options)
     elif browser_name == 'firefox':
+        # код определения языка для Firefox
         fp = webdriver.FirefoxProfile()
         fp.set_preference("intl.accept_languages", user_language)
         browser = webdriver.Firefox(firefox_profile=fp)
     else:
-        raise pytest.UsageError('--browser_name must have the val firefox or chrome and --language val ru or another')
+        raise pytest.UsageError('--browser_name must have the value firefox or chrome')
     yield browser
     browser.quit()
--- test_items.py
+++ test_items.py
@@ -3,6 +3,15 @@ import time
 
 url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
+
 def test_search_add_cart_button(browser):
+    # Given: link to the site 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    # When: I open link in the browser
+    # Then: I see the add to cart button on the page
     browser.get(url)
+
+    # Задержка для оценки работы аргумента --language переданного при запуске скрипта
     time.sleep(30)
+
+    button_add = browser.find_elements(By.CSS_SELECTOR, '.btn-add-to-basket')
+    assert button_add != [], "The 'add to cart' button was not found on the page"
